21f4780eaba3e6800c2cf7605937c34c7bf7848c
Matias <matiasmonti2011@hotmail.com>
2022-04-27 16:10:55 -0300

    Add Fuel and Texts

--- Lunar Lander/Assets/Scripts/ShipMovement.cs
+++ Lunar Lander/Assets/Scripts/ShipMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipMovement : MonoBehaviour
 {
@@ -13,8 +14,13 @@ public class ShipMovement : MonoBehaviour
     private float h;
     private float v;
 
+    [SerializeField] private Slider fuelBar;
+    [SerializeField] private float fuel = 1.0f;
+
     private void Start()
     {
+        fuelBar.value = fuel;
+
         rb = GetComponent<Rigidbody>();
         cam = Camera.main;
     }
@@ -27,8 +33,11 @@ public class ShipMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetAxis("Jump") > 0.01f)
+        if (Input.GetAxis("Jump") > 0.01f && fuel > 0.0f)
         {
+            fuel += -0.1f * Time.deltaTime;
+            fuelBar.value = fuel;
+
             rb.AddForce(transform.up * 6.0f);
 
             if (!botton.isPlaying)
--- Lunar Lander/Assets/Scripts/WinFloor.cs
+++ Lunar Lander/Assets/Scripts/WinFloor.cs
@@ -6,17 +6,23 @@ public class WinFloor : MonoBehaviour
 {
     [SerializeField] private ShipMovement player;
 
+    [SerializeField] private GameManager gameManager;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+
             if (Vector3.Angle(transform.up, player.gameObject.transform.up) < 10)
             {
                 Debug.Log("Ganaste");
+                StartCoroutine(gameManager.GameOverWin());
             }
             else
             {
                 Debug.Log("Perdiste");
+                Destroy(collision.gameObject);
+               StartCoroutine(gameManager.GameOverLose());
             }
         }
     }
