ad93c6d161bf30dc092c7f68e071ee39fbecb2fd
Matias <matiasmonti2011@hotmail.com>
2022-05-04 11:31:10 -0300

    Clean scripts

--- Lunar Lander/Assets/Scripts/WinFloor.cs
+++ Lunar Lander/Assets/Scripts/WinFloor.cs
@@ -1,41 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WinFloor : MonoBehaviour
 {
     [SerializeField] private ShipMovement player;
 
-    [SerializeField] private GameManager gameManager;
+    [SerializeField] private GameObject GameoverLosePanel;
+    [SerializeField] private GameObject GameoverWinPanel;
+    [SerializeField] private GameObject fuelBar;
 
-    [SerializeField] private GameObject GameoverPanel;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player") && player.GetComponent<ShipMovement>())
         {
+            Destroy(player.GetComponent<ShipMovement>());
 
             if (Vector3.Angle(transform.up, player.gameObject.transform.up) < 10)
             {
                 Debug.Log("Ganaste");
-                StartCoroutine(gameManager.GameOverWin());
+                GameoverWinPanel.SetActive(true);
             }
             else
             {
-
-                Destroy(player.GetComponent<ShipMovement>());
-                
                 Debug.Log("Perdiste");
-                GameoverPanel.SetActive(true);
-                StartCoroutine(gameManager.GameOverLose());
-                Destroy(this);
+                GameoverLosePanel.SetActive(true);   
             }
+            fuelBar.SetActive(false);
+            Destroy(this);
         }
 
         if (collision.relativeVelocity.magnitude > 3 && player.GetComponent<ShipMovement>())
         {
             Destroy(player.GetComponent<ShipMovement>());
-            GameoverPanel.SetActive(true);
-            StartCoroutine(gameManager.GameOverLose());
+            fuelBar.SetActive(false);
+            GameoverLosePanel.SetActive(true);
             Destroy(this);
         }
     }
