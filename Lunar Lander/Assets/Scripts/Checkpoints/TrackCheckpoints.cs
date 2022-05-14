83ed800a49a2aa0887216d3acf92bf1da5ca3141
Matias <matiasmonti2011@hotmail.com>
2022-05-14 01:48:23 -0300

    Add checkpoint system

---
+++ Lunar Lander/Assets/Scripts/Checkpoints/TrackCheckpoints.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCheckpoints : MonoBehaviour
+{
+    public event EventHandler OnPlayerCorrectCheckpoint;
+    public event EventHandler OnPlayerWrongCheckpoint;
+
+    private List<CheckpointSingle> checkpointSingles;
+    private int nextCheckpointSingleIndex;
+
+    private void Awake()
+    {
+        Transform checkpointsTransform = transform.Find("Checkpoints");
+
+        checkpointSingles = new List<CheckpointSingle>();
+        foreach (Transform checkpointSingleTransform in checkpointsTransform)
+        {
+            CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
+
+            checkpointSingle.SetTrackCheckpoints(this);
+
+            checkpointSingles.Add(checkpointSingle);
+        }
+
+        nextCheckpointSingleIndex = 0;
+    }
+
+    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
+    {
+        if (checkpointSingles.IndexOf(checkpointSingle) == nextCheckpointSingleIndex)
+        {
+            Debug.Log("correct");
+            nextCheckpointSingleIndex = (nextCheckpointSingleIndex + 1) % checkpointSingles.Count;
+            OnPlayerCorrectCheckpoint?.Invoke(this, EventArgs.Empty);
+
+            CheckpointSingle correctCheckpointSingle = checkpointSingles[nextCheckpointSingleIndex];
+            correctCheckpointSingle.Hide();
+        }
+        else
+        {
+            Debug.Log("Incorrectt");
+            OnPlayerWrongCheckpoint?.Invoke(this, EventArgs.Empty);
+
+            CheckpointSingle correctCheckpointSingle = checkpointSingles[nextCheckpointSingleIndex];
+            correctCheckpointSingle.Show();
+        }
+    }
+}
